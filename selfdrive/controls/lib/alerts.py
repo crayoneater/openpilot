24fb77537c6a9de8c9cfae73564d3aaabc9be01b
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2020-01-05 16:35:47 -0600

    disabled audible enable alert, fan changes, driver monitor timeout

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -64,7 +64,7 @@ ALERTS = [
       "",
       "",
       AlertStatus.normal, AlertSize.none,
-      Priority.MID, VisualAlert.none, AudibleAlert.chimeEngage, .2, 0., 0.),
+      Priority.MID, VisualAlert.none, AudibleAlert.none, .2, 0., 0.),
 
   Alert(
       "disable",
